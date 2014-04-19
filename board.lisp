e7ea2fbf74d38e3e39c2da14cf8eb01a710b97a3
Richie Manis <richie.manis@gmail.com>
2014-04-19 17:05:44 -0400

    Move AI functions to their own file.

--- board.lisp
+++ board.lisp
@@ -56,52 +56,6 @@
 (defun transpose (l)
   (loop for r from 0 to 3 appending (col r l)))
 
-(defun fitness (board-list)
-  (let* ((zeroes (count 0 board-list))
-         (adjacents (count-adjacents board-list))
-         (weighted-adjacents (count-adjacents-weighted board-list))
-         (biggest (apply #'max board-list))
-         (power (floor (log biggest 2))))
-    (+ zeroes
-       #+nil adjacents
-       weighted-adjacents
-       (* 2 power))))
-
-(defun count-adjacents (board-list)
-  (let ((row-adjacents
-	 (loop for row in (rows board-list)
-               summing (count-row-adjacents (remove 0 row))))
-	(column-adjacents
-	 (loop for column in (columns board-list)
-               summing (count-row-adjacents (remove 0 column)))))
-    (+ row-adjacents column-adjacents)))
-
-(defun count-adjacents-weighted (board-list)
-  (let ((row-adjacents-weighted
-         (loop for row in (rows board-list)
-               summing (count-row-adjacents-weighted (remove 0 row))))
-        (column-adjacents-weighted
-         (loop for column in (columns board-list)
-               summing (count-row-adjacents-weighted (remove 0 column)))))
-    (+ row-adjacents-weighted column-adjacents-weighted)))
-
-(defun count-row-adjacents-weighted (nums &optional (acc 0))
-  (let ((first (first nums)))
-    (if nums
-        (count-row-adjacents
-         (cdr nums)
-         (if (eql first (second nums))
-             (+ acc (floor (log first 2)))
-             acc))
-        acc)))
-
-(defun count-row-adjacents (nums &optional (acc 0))
-  (if nums
-      (count-row-adjacents (cdr nums)
-			   (if (eql (first nums) (second nums))
-			       (1+ acc)
-			       acc))
-      acc))
 
 (defun rows (board-list)
   (loop for r from 0 to 3 collecting (row r board-list)))
@@ -149,34 +103,6 @@ is replaced with replacement."
        when pos do (write-string replacement out)
        while pos))) 
 
-;;; Playing on paste.lisp.org
-
-(defun slurp-http (url)
-  (let ((result (trivial-http:http-get url)))
-    (destructuring-bind (status headers stream) result
-      (declare (ignore status))
-      (let ((seq (make-string (read-from-string (cdr (assoc :content-length headers))))))
-	(read-sequence seq stream)
-	seq))))
-
-(defun print-all-moves (url)
-  (let* ((board (slurp-http url))
-	 (directions '(:up :right :left :down))
-	 (boards (mapcar (lambda (x) (move-board x (read-board board)))
-			 directions)))
-    (princ board) (terpri)
-    (loop
-       for direction in directions
-       for possibility in boards
-       do
-	 (princ direction)
-	 (terpri)
-	 (print-board possibility)
-	 (terpri))))
-
-(defun move-http (url direction)
-  (move direction (slurp-http url)))
-
 ;;; Placing a 2 or a 4
 
 (defun place (number position board-list)
@@ -208,73 +134,3 @@ is replaced with replacement."
 (defun mapcar-many (function list)
   (apply #'append (mapcar function list)))
 
-;;; Minimax
-
-(defun determine-slides (board-list)
-  "Calculate the ways to slide the numbers on the board.  Returns a
-list of moves, where each move is a list containing
- - the direction of the move
- - the board after that move is made"
-  (remove-if (lambda (x) (equal (second x) board-list))
-             (mapcar (lambda (direction)
-                       (list direction (move-board direction board-list)))
-                     '(:up :right :down :left))))
-
-(defun higher-future (future-a future-b)
-  (let ((a-score (car future-a))
-        (b-score (car future-b)))
-    (when (and a-score b-score)
-      (if (>= a-score b-score)
-          future-a
-          future-b))))
-
-(defun minimax (board-list &key (search-depth 4) (heuristic #'fitness))
-  "Minimax function that determines the slider's optimal move.
-Returns a list containing
- - The heuristic
- - The path to get there"
-  (if (<= search-depth 0)
-      (list (funcall heuristic board-list) board-list)
-      (let* ((moves (determine-slides board-list))
-             (evaluations
-              (mapcar
-               (lambda (future) ; (:up (0 2 4 ...)
-                 (let ((them (maximin (second future)
-                                      :search-depth (1- search-depth)
-                                      :heuristic heuristic)))
-                   ; them: (SCORE HISTORY)
-                   (list (first them)
-                         (first future)
-                         (second future))))
-               moves)))
-         (if evaluations
-             (let ((answer (reduce #'higher-future evaluations)))
-               (values answer (list 'quote (third answer))))
-             (list 0 :dead)))))
-
-(defun lowest-future (futures)
-  (reduce
-   (lambda (a b)
-     (let ((score-a (first a))
-           (score-b (first b)))
-       (if (<= score-a score-b)
-           a
-           b)))
-   futures))
-
-(defun maximin (board-list &key (search-depth 3) (heuristic #'fitness))
-  "Minimax function that determines the number-placer's optimal move.
-That is, the future with the lowest score returned by the HEURISTIC
-function.  Returns a list containing the minimum score and the path to
-it."
-  (if (<= search-depth 0)
-      (list (funcall heuristic board-list) nil)
-      (let* ((possibilities (all-cpu-moves board-list))
-             (results                   ; ex ((9 :left) (10 :up))
-              (mapcar (lambda (board)
-                        (minimax board
-                                 :search-depth search-depth
-                                 :heuristic heuristic))
-                      possibilities)))
-        (when results
-          (lowest-future results)))))
