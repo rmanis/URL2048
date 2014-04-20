c20ddfe4ea408efeac4b0d55b5826e3719b6fce2
Richie Manis <richie.manis@gmail.com>
2014-04-19 21:47:24 -0400

    Default to an empty board.

--- cgi.lisp
+++ cgi.lisp
@@ -5,5 +5,6 @@
   "Pick up crap from the environment and use it to make something
 wonderful.  Recycling?"
   (let* ((env (http:http-parse-query-string))
-	 (board (gethash "board" env)))
+	 (board (or (gethash "board" env)
+                    "0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 CPU")))
     (princ (html-board board))))
